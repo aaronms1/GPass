c4525232e2356e2ce7e37ee6114906f43e4f3cd6
aaronms1 <amsanon1@protonmail.com>
2023-06-03 20:24:49 -0400

    initcomm1

--- src/test/java/generator/PasswordFactoryTest.java
+++ src/test/java/generator/PasswordFactoryTest.java
@@ -2,43 +2,46 @@ package generator;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.stream.Stream;
+
 public class PasswordFactoryTest {
-    generator.PasswordFactory passwordFactory= new generator.PasswordFactory();
+    PasswordFactory passwordFactory = new PasswordFactory();
 
     @Test
-    public void testMain(){
-        PasswordFactory.main(new java.lang.String[]{"args"});
+    public void testMain() {
+        PasswordFactory.main(new String[]{"args"});
     }
 
     @Test
-    public void testGenerateSecurePassword(){
-        java.lang.String result = PasswordFactory.generateSecurePassword();
-        Assert.assertEquals(result, "replaceMeWithExpectedResult");
+    public void testGenerateSecurePassword_returnsRandomPassword() {
+        String result = PasswordFactory.generateSecurePassword();
+        // test regex to ensure random password is generated
+        Assert.assertTrue(result.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{32}$"));
     }
 
+
     @Test
-    public void testGetRandomSpecialChars(){
-        java.util.stream.Stream<java.lang.Character> result = PasswordFactory.getRandomSpecialChars(0);
+    public void testGetRandomSpecialChars() {
+        Stream<Character> result = PasswordFactory.getRandomSpecialChars(0);
         Assert.assertEquals(result, null);
     }
 
     @Test
-    public void testGetRandomNumbers(){
-        java.util.stream.Stream<java.lang.Character> result = PasswordFactory.getRandomNumbers(0);
+    public void testGetRandomNumbers_returnsRandomNumbers() {
+        Stream<Character> result = PasswordFactory.getRandomNumbers(0);
         Assert.assertEquals(result, null);
     }
 
     @Test
-    public void testGetRandomAlphabets(){
-        java.util.stream.Stream<java.lang.Character> result = PasswordFactory.getRandomAlphabets(0, true);
+    public void testGetRandomAlphabets() {
+        Stream<Character> result = PasswordFactory.getRandomAlphabets(0, true);
         Assert.assertEquals(result, null);
     }
 
     @Test
-    public void testToString(){
-        java.lang.String result = passwordFactory.toString();
+    public void testToString() {
+        String result = passwordFactory.toString();
         Assert.assertEquals(result, "replaceMeWithExpectedResult");
     }
 }
-
-//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
